27597a6a404c5c21a53b944c5295b55117da4a49
mohammadmunnamia0 <mohammadmunnamia0@gmail.com>
2025-06-19 17:14:01 +0600

    New project with the svelte

---
+++ src/lib/sanity/Studio.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import { defineConfig, Studio } from 'sanity';
+  import config from '$studio/sanity.config'; // adjust based on your setup
+
+  let studio: typeof Studio;
+
+  onMount(async () => {
+    const mod = await import('sanity');
+    studio = mod.Studio;
+  });
+</script>
+
+{#if studio}
+  <svelte:component this={studio} config={config} />
+{:else}
+  <p>Loading Studio...</p>
+{/if}
---
+++ src/routes/(studio)/[...slug]/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+  import Studio from '$lib/sanity/Studio.svelte';
+</script>
+
+<Studio />
